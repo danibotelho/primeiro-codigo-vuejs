4bd5f3fdb1148db2f4fb013b4d1b15d713ca7cfa
danibotelho <danibl26@gmail.com>
2024-03-07 22:49:30 -0300

    aprendendo o recurso watches

--- Terceiro modulo vuejs/terceiroModulo/src/App.vue
+++ Terceiro modulo vuejs/terceiroModulo/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import ReatividadeVue from "./components/Reatividade.vue";
 import Computed from "./components/Computed.vue";
+import Watcher from "./components/Watcher.vue";
 </script>
 
 <template>
   <main>
     <!-- <ReatividadeVue /> -->
-    <Computed />
+    <!-- <Computed /> -->
+    <Watcher />
   </main>
 </template>
