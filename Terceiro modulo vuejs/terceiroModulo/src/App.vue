66bbf6fade3ea6e82166831be797683e88f2893f
danibotelho <danibl26@gmail.com>
2024-03-07 22:39:29 -0300

    terceiro modulo, reactive , computed

---
+++ Terceiro modulo vuejs/terceiroModulo/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ReatividadeVue from "./components/Reatividade.vue";
+import Computed from "./components/Computed.vue";
+</script>
+
+<template>
+  <main>
+    <!-- <ReatividadeVue /> -->
+    <Computed />
+  </main>
+</template>
